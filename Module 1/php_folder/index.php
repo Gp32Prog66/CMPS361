13e9e26d437171f410bd22e4bfed74874a511a46
Gp32Prog66 <71804120+Gp32Prog66@users.noreply.github.com>
2024-09-08 18:06:19 -0400

    Update index.php

--- Module 1/php_folder/index.php
+++ Module 1/php_folder/index.php
@@ -5,38 +5,47 @@
 
 </head>
 
+<style>
+        body{
+    background-image: url("background/SharpsHillView.jpg");
+        }
+    </style>
+
+
 
 <!-- Using Google ajax API-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js">
 
 </script>
 
-<div class="center">
 
-    <body>
+<div class="center">
+<div class = "caption">            
+
         <h1> Hey Everyone, John Szoszorek Here!</h1>
         <p1> I am a Senior at Point Park University. My major is Applied Computer Science. 100% Yinzer</p1>
-    <div class = "caption">
+    
+<div class="backOne">
+
         <h2> Assignment Links: </h2>
-        <p><a href="http://jrszosz.it.pointpark.edu/Assignment%202/form.html">Module 2</a></p>
+        <p><a href="https://github.com/Gp32Prog66/Assignment_2/tree/dev">Module 2</a></p>
         <p><a href="https://github.com/Gp32Prog66/Assignment_3">Module 3</a></p>
         <p><a href="http://167.88.242.32/PHP_Page/phpinfo.php">PHP MyInfo</a></p>
         <p><a href="http://167.88.242.32/phpinfo.php">Alternative MyInfo</a></p>
-        </div>
+        
 
-        <h2> What I have learned in Week 3</h2>
+        <h2> Other PHP Files</h2>
 
         <ul>
-            <li> 1. Learned How to Install NodeJS</li>
-            <li> 2. Ran my First Program Using Node JS via Google Chrome</li>
-            <li> 3. Installing Packages via the Node Package Manager</li>
-            <li> 4. Display the Results of my Program Via a Port Number</li>
+            <li><a href="firstPHP.php">1. firstPHP.php </a></li>
+            <li><a href="webpage.php"> 2. webpage.php</a></li>
+            
         </ul>
 
 
         <h2> Picture of Me</h2>
 
-        <img src="pic.jpg" width=300 alt="John Szoszorek">
+        <img src="background/Suit.jpg" width=300 alt="John Szoszorek">
 
         <h2> Interests</h2>
 
@@ -60,34 +69,40 @@
 
         </ol>
 
-        <h2> Demonstrations</h2>
-        <p><a href="http://jrszosz.it.pointpark.edu/Animation/First Animation.html">Basic Animation</a></p>
-
+        <div class="contact">
         <h2> How to Contact Me</h2>
+
+        <div class ="">
         <p2> Email: jrszosz@pointpark.edu</p2>
         <p><a href="https://github.com/Gp32Prog66">My Github Account</a></p>
-
+        </div>
 
         <p> This is a test site where I am transferring the contents to PHP </p>
 </div>
+</div>
 
 <!-- PHP Scripting-->
+<td width = '40' class = 'center'>
 <?php
-echo "Using the echo command. Creates an echo chamber";
+
+echo "Using the echo command. Creates an echo chamber\n"; 
 #Variable
 $name = "John";
 echo $name;
 
-$string = "Welcome! Enjoy your visit.";
+$string = "\nWelcome! Enjoy your visit.";
 $int = 42;
 
 echo $string;
-echo "The meaning of life is ";
+echo "\nThe meaning of life is ";
 echo $int;
 
 $guest = "Viewer";
+
 ?>
-<p> Say Hello to our special guest <?php echo $guest ?>, and hope you enjoy your visit! </p>
+
+<p> Say Hello to our special guest <?php echo $guest ?>, and I hope you enjoy your visit! </p>
+</td>
 </body>
 
 </html>
